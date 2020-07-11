6ae5e68503bd3a05951c65b2afb75db3834a9612
mohanson <mohanson@outlook.com>
2020-07-11 21:56:02 +0800

    Loop forever

--- baidusinking.py
+++ baidusinking.py
@@ -9,7 +9,7 @@ def get_current_index() -> int:
 
 def inc_current_index():
     s = hex(get_current_index() + 1)[2:]
-    s = f'{s:0>4}'
+    s = f'{s:0>8}'
     with open('res/i', 'w') as f:
         f.write(s)
 
@@ -17,12 +17,12 @@ def inc_current_index():
 if not os.path.exists('res'):
     os.mkdir('res')
     with open('res/i', 'w') as f:
-        f.write('0000')
+        f.write('00000000')
 
-
-next_index = hex(get_current_index() + 1)[2:]
-next_index = f'{next_index:0>4}'
-
-subprocess.call(f'cat /dev/urandom | head -c 1073741824 > res/{next_index}.zip', shell=True)
-subprocess.call(f'BaiduPCS-Go upload --nosplit res/{next_index}.zip /肥大', shell=True)
-subprocess.call(f'rm res/{next_index}.zip')
+for _ in range(1 << 32):
+    next_index = hex(get_current_index() + 1)[2:]
+    next_index = f'{next_index:0>8}'
+    subprocess.call(f'cat /dev/urandom | head -c 67108864 > res/{next_index}.zip', shell=True)
+    subprocess.call(f'BaiduPCS-Go upload --nosplit res/{next_index}.zip /肥大', shell=True)
+    subprocess.call(f'rm res/{next_index}.zip', shell=True)
+    inc_current_index()
