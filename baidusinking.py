572711341b2849e3b3b0671cd3d7885af1644083
mohanson <mohanson@outlook.com>
2020-07-11 22:22:59 +0800

    Remove --nosplit flag

--- baidusinking.py
+++ baidusinking.py
@@ -23,6 +23,6 @@ for _ in range(1 << 32):
     next_index = hex(get_current_index() + 1)[2:]
     next_index = f'{next_index:0>8}'
     subprocess.call(f'cat /dev/urandom | head -c 67108864 > res/{next_index}.zip', shell=True)
-    subprocess.call(f'BaiduPCS-Go upload --nosplit res/{next_index}.zip /肥大', shell=True)
+    subprocess.call(f'BaiduPCS-Go upload res/{next_index}.zip /肥大', shell=True)
     subprocess.call(f'rm res/{next_index}.zip', shell=True)
     inc_current_index()
